268b4b212db53e94322492bb6911241704ee4f9d
derickcastillo38 <9derick@gmail.com>
2018-06-22 01:58:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,10 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |name|
-    puts name 
+    puts name
+  end
+
+  assign_rooms(attendees).each do |room|
+    puts room
   end
 end
